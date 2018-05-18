3e4ec9572b72c483794040d9f38e04df840ca37a
vlad <vlad@localhost.localdomain>
2018-05-18 15:29:44 +0300

    преобразовываем значения в таймштамп только через авк

--- test_part.sh
+++ test_part.sh
@@ -1,14 +1,16 @@
 LOGFILE="/bin/vlad/access.log"
 tmp=""
-count_send_data(){
+get_timestamp_from_log(){
 echo ""
 ## убрать sed засунуть его функционал в awk
-tail -100 $LOGFILE |awk '{print $4}' |sed -e 's/^.//' -e 's,/,-,g' -e 's,:, ,'|date -f - +"%s"
-#$ awk 'BEGIN{FS="[][]"}{system("date \"+%s\" -d \""gensub("/", " ", "G", gensub(":", " ", "1", $2))"\"")}' file
-#echo " июль семнадцатого"
-date -d "26-Jul-2017 03:35:22" +"%s"
-#echo " Today"
-#date +"%s"
+#cat $LOGFILE |awk '{print $4}' |sed -e 's/^.//' -e 's,/,-,g' -e 's,:, ,'|date -f - +"%s"
+# cтрока должна переводить в таймштамп авк $
+awk 'BEGIN{FS="[][]"}{system("date \"+%s\" -d \""gensub("/", " ", "G", gensub(":", " ", "1", $2))"\"")}'
 }
-
-count_send_data
+count_number_of_requests_per_hour(){
+cat $LOGFILE \
+|get_timestamp_from_log\
+|awk '{}'
+}
+count_number_of_requests_per_hour
+#get_timestamp_from_log
