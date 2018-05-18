9958c87173dd0a0895e49961a7066594d0a4bd5d
vlad <vlad@localhost.localdomain>
2018-05-18 17:20:50 +0300

    Отбирает по времени до 100 штук

--- test_part.sh
+++ test_part.sh
@@ -1,5 +1,6 @@
 LOGFILE="/bin/vlad/access.log"
 tmp=""
+TIME_SEPARATOR="1000"
 get_timestamp_from_log(){
 echo ""
 ## убрать sed засунуть его функционал в awk
@@ -8,9 +9,16 @@ echo ""
 awk 'BEGIN{FS="[][]"}{system("date \"+%s\" -d \""gensub("/", " ", "G", gensub(":", " ", "1", $2))"\"")}'
 }
 count_number_of_requests_per_hour(){
+#нужно убрать греп первого таймштампа отсюда и сделать его более адекватно
+first_timestamp=$(head -1 $LOGFILE |awk 'BEGIN{FS="[][]"}{system("date \"+%s\" -d \""gensub("/", " ", "G", gensub(":", " ", "1", $2))"\"")}')
+#tmp=$(($first_timestamp + $TIME_SEPARATOR))
 cat $LOGFILE \
 |get_timestamp_from_log\
-|awk '{}'
+|awk -v ts=$TIME_SEPARATOR -v ft=$first_timestamp '{if ($0 <= ft+ts){{counter++}{print "CURENT " $0 " less then" ft+ts " counter =" counter}} else{print "oops" $0}}  END{print counter}'
 }
+#for ((i=0; i<10000; i++)){
+#  echo " $i "
+#}
+
 count_number_of_requests_per_hour
 #get_timestamp_from_log
